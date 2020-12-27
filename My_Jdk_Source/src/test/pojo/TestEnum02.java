3f3ade5422406abd6991792e255f479b27458e1d
suaf <suaf@bsfit.com.cn>
2020-12-27 21:28:27 +0800

    xuexi

--- My_Jdk_Source/src/test/pojo/TestEnum02.java
+++ My_Jdk_Source/src/test/pojo/TestEnum02.java
@@ -4,12 +4,16 @@ package test.pojo;
  * private static TestEnum02 SPRING = new TestEnum02("春天");简化为SPRING("春天");
  * */
 public class TestEnum02 {
-    private static TestEnum02 SPRING = new TestEnum02("春天");
-    private static TestEnum02 SUMMER = new TestEnum02("夏天");
-    private static TestEnum02 FALL = new TestEnum02("秋天");
-    private static TestEnum02 WINTER = new TestEnum02("冬天");
-    private String name;
+    //枚举私有化new出来，可以给final修饰
+    private static final TestEnum02 SPRING = new TestEnum02("春天");
+    private static final TestEnum02 SUMMER = new TestEnum02("夏天");
+    private static final TestEnum02 FALL = new TestEnum02("秋天");
+    private static final TestEnum02 WINTER = new TestEnum02("冬天");
 
+    //声明属性，私有化
+    private final String name;
+
+    //构造方法私有化
     private TestEnum02(String name) {
         this.name = name;
     }
